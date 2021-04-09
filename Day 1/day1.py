2122075cec5d1e6d6d3df433fb471a534a5457f8
Chris Gu <cgu2022@pingry.org>
2021-04-09 11:40:58 -0400

    Added Day 2 HW

---
+++ Day 1/day1.py
@@ -0,0 +1,11 @@
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+img = cv.imread('sample.png')
+print(img.item(0,0,2))
+plt.imshow(img)
+plt.show()
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
